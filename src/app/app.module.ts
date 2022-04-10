17183e6aab5d08cf1a12b88b828d92938d56492b
Ritul Daryan <ritul.daryan.205@gmail.com>
2022-04-10 17:25:51 +0530

    Basic Application Setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { AuthComponent } from './pages/auth/auth.component';
+import { FeedsComponent } from './pages/feeds/feeds.component';
+import { ChatsComponent } from './pages/chats/chats.component';
+import { NotfoundComponent } from './pages/notfound/notfound.component';
+import { PostsComponent } from './components/posts/posts.component';
+import { FriendsComponent } from './components/friends/friends.component';
+import { ProfileComponent } from './components/profile/profile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    AuthComponent,
+    FeedsComponent,
+    ChatsComponent,
+    NotfoundComponent,
+    PostsComponent,
+    FriendsComponent,
+    ProfileComponent,
   ],
   imports: [
     BrowserModule,
