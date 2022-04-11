88e32b0c8df1ccbc67c756ad3f4ffb8b5d4847f4
Ritul Daryan <ritul.daryan.205@gmail.com>
2022-04-11 16:50:39 +0530

    Auth PageUI Finished

--- src/app/pages/auth/auth.component.ts
+++ src/app/pages/auth/auth.component.ts
@@ -1,14 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 
+
+
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.component.html',
   styleUrls: ['./auth.component.scss']
 })
 export class AuthComponent implements OnInit {
-
+    signIn = true;
+    error  = null;
   constructor() { }
 
+  changeOption(prevStatus:boolean){
+      this.signIn = prevStatus;
+  }
   ngOnInit(): void {
   }
 
